d4c872d01f04cc7af1075af8cb89d24848f28c0f
hsx0306 <1317hsb@gmail.com>
2024-01-20 02:00:17 +0900

    update express use file

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/"));
 
-
+app.use(express.static(path.join(__dirname, "/")));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, ""));
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 const loginRegisterRoutes = require('./routes/login_register');
